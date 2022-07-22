6423d9c3e6719496d5194c67220809b65507fa47
alskumyn <82923066+bdaoust28@users.noreply.github.com>
2022-07-22 00:21:03 -0700

    Update strip.py
    Cleaned up strip process to utilize enumeration and improve readability

--- strip.py
+++ strip.py
@@ -3,12 +3,12 @@ import csv
 
 # Step 1: strip all items of trailing whitespace
 def strip():
-    items = csv.reader(open('items.csv', newline='', encoding='utf8'), delimiter=',')
+    joblist = csv.reader(open('items.csv', newline='', encoding='utf8'), delimiter=',')
     with open('items-stripped.csv', 'w+', newline='') as s_items:
         writer = csv.writer(s_items, delimiter=',')
-        for row in items:                   # row traversal
-            for i in range(len(row)):       # item traversal
-                row[i] = row[i].strip()     # strip whitespace
+        for row in joblist:                 # row traversal
+            for i, item in enumerate(row):  # item traversal
+                row[i] = item.strip()       # strip whitespace
             writer.writerow(row)            # write new row to 'items_stripped.csv'
 
 if __name__ == '__main__':
